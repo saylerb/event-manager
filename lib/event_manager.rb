ccb66b12f2beeed6274820dbc24a9a6b0b0a9b1f
saylerb <sayler.b@gmail.com>
2016-04-17 19:51:51 -0600

    transferring to CSV library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,14 @@
+require "csv"
+
 puts "EventManager Initialized"
 
-File.readlines("event_attendees.csv").each_with_index do |line, index|
 
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+
+
+contents.each do |row|
+  name = row[:first_name]
   puts name
-
 end
 
 
