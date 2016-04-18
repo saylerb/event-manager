25433a68c54e65700411910cdf601e3f404f84cc
saylerb <sayler.b@gmail.com>
2016-04-17 20:06:02 -0600

    create new method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,18 @@
 require "csv"
 
+
+def clean_zipcode(zipcode)
+  if zipcode.nil? 
+     "00000"
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  elsif zipcode.length < 5
+    zipcode.rjust(5, "0")
+  else
+    zipcode
+  end
+end
+
 puts "EventManager Initialized"
 
 
@@ -8,21 +21,8 @@ contents = CSV.open("event_attendees.csv", headers: true, header_converters: :sy
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
-
-  if zipcode.nil? 
-    zipcode = "00000"
-  elsif zipcode.length == 5
-    zipcode 
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, "0")
-  else
-  end
-    
+  zipcode = clean_zipcode(row[:zipcode])
 
   puts "#{name} #{zipcode}"
 end
-
 
