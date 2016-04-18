e11fd4cc384ade13284396dc5f54d5bf1a36751e
saylerb <sayler.b@gmail.com>
2016-04-17 20:34:06 -0600

    iteration 3 done

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,11 +4,20 @@ require "sunlight/congress"
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
+
+
+def legislator_by_zipcode(zipcode)
+
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  legislator_names = legislators.map do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+  legislator_names.join(", ")
+end
+
 
 puts "EventManager Initialized"
 
@@ -20,13 +29,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = []
-  legislators.each do |legislator|
-    legislator_name = "#{legislator.first_name} #{legislator.last_name}"
-    legislator_names.push(legislator_name)
-  end
+  legislator_names = legislator_by_zipcode(zipcode)
 
   puts "#{name} #{zipcode} #{legislator_names}"
 end
